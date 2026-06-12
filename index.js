9afaff4bb318e431760a73c2274d12a96527b2b7
manoj7 <kingmanumanoj0@gmail.com>
2026-06-12 21:56:41 +0530

    server connecting code part -1

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const PORT = 3000
 // (we'll explain  middleware deeply on Day 6)
 app.use(express.json())
 
+
+//
 app.get('/', (req, res) => {
     res.status(200).json({
         message: "Welcome to my Express server!",
@@ -15,3 +17,35 @@ app.get('/', (req, res) => {
 
     })
 })    
+
+app.get('/about', (req, res) => {
+    res.status(200).json({
+        name : "Manoj.P",
+        role : "Backend Developer (in training)",
+        day : 3
+    })
+})
+
+app.get('/skills', (req, res) => {
+    res.status(200).json({
+        skills : ["HTTP", "Node.js", "Express", "REST APIs"]
+    })
+})
+
+
+app.get('/time', (req, res) => {
+    res.status(200).json({
+        currentTime: new Date().toLocaleString()
+    })
+})
+
+app.use((req, res) => {
+    res.status(404).json({
+        error: "Route not found",
+        path: req.url
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
