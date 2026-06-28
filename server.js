98d48fe42f41dee23cfbc8e061fb77b9168fe844
manoj7 <kingmanumanoj0@gmail.com>
2026-06-28 22:34:38 +0530

    backend tuturial

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+
+app.set("view engine" , "ejs")
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!  999'))
+app.get('/', (req, res) => res.render("homepage"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
